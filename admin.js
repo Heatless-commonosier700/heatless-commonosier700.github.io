566b07e6b529c529734c82627a3d4f01eb73b082
tetxc <teastates12@gmail.com>
2026-07-12 20:30:01 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,43 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    doc,
+    updateDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
+// Paste your firebaseConfig here
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const select = document.getElementById("playerSelect");
+
+const players = [];
+
+const snapshot = await getDocs(collection(db, "players"));
+
+snapshot.forEach(d => {
+    players.push({
+        id: d.id,
+        ...d.data()
+    });
+
+    const option = document.createElement("option");
+    option.value = d.id;
+    option.textContent = d.data().name;
+
+    select.appendChild(option);
+});
+
+document.getElementById("saveBtn").onclick = async () => {
+
+    const id = select.value;
+    const points = Number(document.getElementById("points").value);
+
+    await updateDoc(doc(db, "players", id), {
+        points: points
+    });
+
+    alert("Saved!");
+};
